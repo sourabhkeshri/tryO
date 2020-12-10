76056fa09681f82e5fa938c9664573db6070163c
sourabh keshri <sourabhkeshri02@gmail.com>
2020-12-10 16:09:40 +0530

    Creating sweetShop basic

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,33 @@
+class RecipesController < ApplicationController
+	before_action :find_recipe, only: [:show,:edit,:update,:destroy]
+
+	def index
+		@recipes=Recipe.all
+	end
+
+	def show
+	end
+
+	def new 
+		@recipe=Recipe.new
+	end
+
+	def create
+		@recipe=Recipe.new(recipe_params)
+		if @recipe.save
+			redirect_to @recipe, notice: "created!"
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:title, :desc)
+	end
+
+	def find_recipe
+		@recipe=Recipe.find(params[:id])
+	end
+end
